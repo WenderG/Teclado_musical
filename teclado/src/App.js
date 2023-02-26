c106aefa6ff702f96d08f608d041e7bffff36b2c
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-02-26 09:21:25 -0300

    :sparkles: feat: Adicionando Footer.js

--- teclado/src/App.js
+++ teclado/src/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Teclado from "./components/Teclado"
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="app-container">
       <Teclado />
+      <Footer/>
     </div>
   )
 }
---
+++ teclado/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div>
+            <p>Desenvolvido por @WenderG - 2023</p>
+        </div>
+    )
+}
+
+export default Footer
