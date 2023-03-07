ce7d5500e3bb3b77f4635bcd277e46ebda4b9d67
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-03-07 19:55:08 -0300

    build(BlackKey): create component BlackKey

---
+++ src/components/BlackKey/BlackKey.jsx
@@ -0,0 +1,14 @@
+import './BlackKey.css'
+
+export function BlackKey() {
+  return (
+    <div className="piano-black-keys flex gap">
+      <div className="black-key">2</div>
+      <div className="black-key">3</div>
+      <div className="black-key">.</div>
+      <div className="black-key">5</div>
+      <div className="black-key">6</div>
+      <div className="black-key">7</div>
+    </div>
+  )
+}
