7d9fc00249adfd7b5e6d3f73102a42161850a36b
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-03-07 19:54:44 -0300

    build(WhiteKey): create component WhiteKey

---
+++ src/components/WhiteKey/WhiteKey.jsx
@@ -0,0 +1,15 @@
+import './WhiteKey.css'
+
+export function WhiteKey() {
+  return (
+    <div className="piano-white-keys flex">
+      <div className="white-key">Q</div>
+      <div className="white-key">W</div>
+      <div className="white-key">E</div>
+      <div className="white-key">R</div>
+      <div className="white-key">T</div>
+      <div className="white-key">Y</div>
+      <div className="white-key">U</div>
+    </div>
+  )
+}
