4dd780376932f97840d1d707cca7efa528255e1c
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-03-11 20:44:04 -0300

    refactor(WhiteKey): change static to dynamic component

--- src/components/WhiteKey/WhiteKey.jsx
+++ src/components/WhiteKey/WhiteKey.jsx
@@ -1,55 +1,20 @@
-import * as Tone from 'tone'
-import { useEffect, useRef } from 'react';
+import { forwardRef } from 'react';
 
 import './WhiteKey.css'
 
-export function WhiteKey() {
-  const synth = new Tone.Synth().toDestination();
-  const keyRef = useRef(null);
-
-  const keys = {
-    'q': () => synth.triggerAttackRelease("C3", "1n"),
-    'w': () => synth.triggerAttackRelease("D3", "2n")
-  }
-
-  useEffect(() => {
-    keyRef.current.focus();
-  }, [])
-
-  function keyPress(event) {
-    if (keys.hasOwnProperty(event.key)) {
-      const currentKey = keys[event.key];
-      currentKey();
-    }
-
-    return
-  }
+export const WhiteKey = forwardRef(function (props, ref) {
+  const { text, tabIndex, handleClick, onKeyDown, onBlur } = props;
 
   return (
-    <div className="piano-white-keys flex">
-      <button
-        className="white-key"
-        ref={keyRef}
-        tabIndex={1}
-        onKeyDown={keyPress}
-        onBlur={() => keyRef.current.focus()}
-      >
-        Q
-      </button>
-      <button
-        className="white-key"
-        tabIndex={2}
-        ref={keyRef}
-        onKeyDown={keyPress}
-        onBlur={() => keyRef.current.focus()}
-      >
-        W
-      </button>
-      <div className="white-key">E</div>
-      <div className="white-key">R</div>
-      <div className="white-key">T</div>
-      <div className="white-key">Y</div>
-      <div className="white-key">U</div>
-    </div>
+    <button
+      className="white-key"
+      ref={ref}
+      tabIndex={tabIndex}
+      onClick={handleClick}
+      onKeyDown={onKeyDown}
+      onBlur={onBlur}
+    >
+      {text}
+    </button>
   )
-}
+})
