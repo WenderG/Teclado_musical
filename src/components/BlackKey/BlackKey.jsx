beb98c1236eb8bf5f0d8d1c79f9f025e3e495d4c
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-03-11 20:44:38 -0300

    refactor(BlackKey): change static to dynamic component

--- src/components/BlackKey/BlackKey.jsx
+++ src/components/BlackKey/BlackKey.jsx
@@ -1,14 +1,20 @@
+import { forwardRef } from 'react';
+
 import './BlackKey.css'
 
-export function BlackKey() {
+export const BlackKey = forwardRef(function (props, ref) {
+  const { text, tabIndex, handleClick, onKeyDown, onBlur } = props;
+
   return (
-    <div className="piano-black-keys flex gap">
-      <div className="black-key">2</div>
-      <div className="black-key">3</div>
-      <div className="black-key">.</div>
-      <div className="black-key">5</div>
-      <div className="black-key">6</div>
-      <div className="black-key">7</div>
-    </div>
+    <button
+      className="black-key"
+      ref={ref}
+      tabIndex={tabIndex}
+      onClick={handleClick}
+      onKeyDown={onKeyDown}
+      onBlur={onBlur}
+    >
+      {text}
+    </button>
   )
-}
+})
